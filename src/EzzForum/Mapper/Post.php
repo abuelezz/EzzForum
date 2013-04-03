0f70a8d86b79613e364c71bb0f17071ee475db88
Mohammad Abuelezz <aboel3zz@gmail.com>
2013-04-04 01:27:18 +0300

    insert data -- not completed :(

--- src/EzzForum/Mapper/Post.php
+++ src/EzzForum/Mapper/Post.php
@@ -2,20 +2,79 @@
 
 namespace EzzForum\Mapper;
 
-use Zend\Db\Adapter;
-use EzzForum\Entity\Post;
-
 class Post {
 
     protected $dbAdapter;
+    protected $options;
 
-    public function persist(Post $post);
-
-    public function getPaginatorAdapter();
-
-    public function setDbAdapter(Adapter $dbAdapter) {
-        $this->dbAdapter = $dbAdapter;
+    /**
+     * @see ServiceManager in Module.php
+     * @param type $dbAdapter
+     * @param \EzzForum\Mapper\PostDbAdapterMapperOptions $options
+     */
+    public function __construct($dbAdapter, \EzzForum\Mapper\PostDbAdapterMapperOptions $options) {
+        $this->setDbAdapter($dbAdapter);
     }
 
-    public function getDbAdapter();
+    public function setDbAdapter($adapter) {
+        $this->dbAdapter = $adapter;
+    }
+
+    public function getDbAdapter() {
+        return $this->dbAdapter;
+    }
+
+    /**
+     * save or update data
+     * @todo check to save or update
+     * @param type $entity
+     */
+    public function persist($entity) {
+        $this->insert($entity);
+    }
+
+    /**
+     * @todo add select query
+     * @return \Zend\Paginator\Adapter\DbSelect
+     */
+    public function getPaginatorAdapter() {
+        return new \Zend\Paginator\Adapter\DbSelect($select, $this->getDbAdapter());
+    }
+
+    protected function insert($entity) {
+
+        $hydrator = $this->getHydrator();
+        $set = $hydrator->extract($entity);
+
+        $sql = new Sql($this->getDbAdapter(), $this->getTableName());
+        $insert = $sql->insert();
+        $insert->values($set);
+
+        $statement = $sql->prepareStatementForSqlObject($insert);
+        $result = $statement->execute();
+        $lastInsertValue = $this->getWriteDbAdapter()->getDriver()->getConnection()->getLastGeneratedValue();
+
+        return $lastInsertValue;
+    }
+
+    protected function setOptions($options) {
+        $this->options = $options;
+    }
+
+    protected function getOptions() {
+        return $this->options;
+    }
+
+    protected function getHydrator() {
+        return $this->getOptions()->getHydrator();
+    }
+
+    protected function getTableName() {
+        return $this->getOptions()->getTableName();
+    }
+
+    protected function setTableName($tableName) {
+        $this->tableName = $tableName;
+    }
+
 }

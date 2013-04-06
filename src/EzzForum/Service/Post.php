10e080ae9119ef04dc0930c52f5633a677e57185
Mohammad Abuelezz <aboel3zz@gmail.com>
2013-04-07 00:30:50 +0300

    done with the actual work of Post service, I need to handle all the events (create, pre.create, post.create)

--- src/EzzForum/Service/Post.php
+++ src/EzzForum/Service/Post.php
@@ -6,24 +6,86 @@
  * @author Mohammad Abuelezz <mohammad@abuelezz.com>
  */
 
-namespace ezzModule\Service;
+namespace ezzForum\Service;
 
-use EzzForum\Mapper\Post;
+//use EzzForum\Mapper\Post;
 
 class Post {
 
     protected $mapper;
+    protected $postForm;
+    protected $entityPrototype;
+    protected $hydrator;
 
-    public function getPaginator();
+    public function __construct(\EzzForum\Mapper\Post $mapper, $entityPrototype, \EzzForum\Entity\PostHydrator $hydrator) {
+        $this->setMapper($mapper);
+        $this->setEntityPrototype($entityPrototype);
+        $this->setHydrator($hydrator);
+    }
 
-    public function persist($data);
+    public function getPaginator() {
+        echo __FUNCTION__;
+    }
 
-    public function setMapper(Post $mapper) {
+    public function persist($entity, $data = null) {
+        if (!is_object($entity)) {
+            if (is_array($entity)) {
+                $data = $entity;
+                $entity = $this->createEntityFromArray($entity);
+            } else {
+                throw new \Exception("Not an entity");
+            }
+        }
+
+        $mapper = $this->getMapper();
+        $mapper->persist($entity);
+
+        return $event;
+    }
+
+    public function setMapper(\EzzForum\Mapper\Post $mapper) {
         $this->mapper = $mapper;
     }
 
     public function getMapper() {
         return $this->mapper;
+    }
+
+    protected function getPostForm() {
+        if (null === $this->postForm) {
+            $this->postForm = $this->getServiceManager()->get('EzzForum\Form\Post');
+        }
+        return $this->postForm;
+    }
+
+    public function setPostForm(Form $postForm) {
+        $this->postForm = $postForm;
+        return $this;
+    }
+
+    public function getEntityPrototype() {
+        return $this->entityPrototype;
+    }
+
+    public function setEntityPrototype($entityPrototype) {
+        $this->entityPrototype = $entityPrototype;
+    }
+
+    public function getHydrator() {
+        return $this->hydrator;
+    }
+
+    public function setHydrator($hydrator) {
+        $this->hydrator = $hydrator;
+    }
+
+    public function createEntityInstance() {
+        return clone $this->getEntityPrototype();
+    }
+
+    public function createEntityFromArray(array $data) {
+        $entity = $this->getHydrator()->hydrate($data, $this->createEntityInstance());
+        return $entity;
     }
 
 }
